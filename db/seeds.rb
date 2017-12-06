ac71013b9b3d18acc285b0e9f3ffe3f26af57fa1
Alexandre Cuillerier <ac@pierlis.com>
2017-12-06 16:54:39 +0100

    Team view : css

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,12 @@ Team.create(name: 'Finlande', short_name: 'FIN')
 
 
 # ostersund 2017-18 :
-# - relais simple mixte : 
-# - relais mixte : 
-# - individuel femmes : 
-# - individuel hommes : 
-# - sprint femmes : 
-# - sprint hommes :
+# - relais simple mixte : https://www.lequipe.fr/Biathlon/Actualites/Le-resume-video-du-relais-mixte-simple-d-ostersund/854059
+# - relais mixte : https://www.lequipe.fr/Biathlon/Actualites/Le-resume-video-du-relais-mixte-d-ostersund/854094
+# - individuel femmes : https://www.lequipe.fr/lachainelequipe/video/evenements/biathlon-replay-l-individuel-femmes-d-ostersund/x6attlu
+# - individuel hommes : https://www.lequipe.fr/lachainelequipe/video/evenements/biathlon-replay-l-individuel-hommes-d-ostersund/x6avrhd
+# - sprint femmes : https://www.lequipe.fr/lachainelequipe/video/evenements/biathlon-replay-sprint-femmes-ostersund/x6axram
+# - sprint hommes : https://www.lequipe.fr/lachainelequipe/video/evenements/biathlon-replay-sprint-hommes-ostersund/x6azf49
 # - poursuite femmes : https://www.lequipe.fr/lachainelequipe/video/evenements/biathlon-replay-poursuite-femmes-ostersund/x6azril
 # - poursuite hommes : https://www.lequipe.fr/lachainelequipe/video/evenements/biathlon-replay-poursuite-hommes-ostersund/x6azv23
 
